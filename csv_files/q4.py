d5ccd3d034c18a4a6df13f301124c97f59d997af
Dani <danisteele1984@gmail.com>
2022-11-12 10:46:32 +1100

    csv q4 tidy up formatting

--- csv_files/q4.py
+++ csv_files/q4.py
@@ -1,3 +1,29 @@
+
+##### Pt. 1 Using the colours_20.csv file #####
+import csv
+
+def colour_finder(reader, colour):
+    count = 0
+    for line in reader:
+        for word in line:
+            if colour in word:
+                count += 1
+    return(count)
+
+with open("colours_20.csv") as csv_file:
+    reader = csv.reader(csv_file)
+    print(f'Red: {colour_finder(reader, "red")}', f'Green: {colour_finder(reader, "Green")}', f'Blue: {colour_finder(reader, "Blue")}', f'Yellow: {colour_finder(reader, "Yellow")}')
+
+or: 
+    print(f'Red: {colour_finder(reader, "red")}')
+    print(f'Green: {colour_finder(reader, "green")}'),
+    print(f'Blue: {colour_finder(reader, "blue")}'),
+    print(f'Yellow: {colour_finder(reader, "yellow")}')
+
+
+##### Pt. 2 using the colours_213.csv file #####
+# Note: This one I don't know if I got working as it should be the output matches my word-count 
+# But it doesn't match what the exercise said the output should be. 
 import csv
 
 def colour_finder(reader, colour):
@@ -10,29 +36,8 @@ def colour_finder(reader, colour):
 with open("colours_213.csv") as csv_file:
     reader = csv.reader(csv_file)
     print(colour_finder(reader, "Red"))
-    #for line in reader(line.lower()):
+    for line in reader(line.lower()):
 
-    #print(f'Red: {colour_finder(reader, "red")}', f'Green: {colour_finder(reader, "Green")}', f'Blue: {colour_finder(reader, "Blue")}', f'Yellow: {colour_finder(reader, "Yellow")}')
+    print(f'Red: {colour_finder(reader, "red")}', f'Green: {colour_finder(reader, "Green")}', f'Blue: {colour_finder(reader, "Blue")}', f'Yellow: {colour_finder(reader, "Yellow")}')
 
 
-
-##### Pt. 1 #####
-# import csv
-
-# def colour_finder(reader, colour):
-#     count = 0
-#     for line in reader:
-#         for word in line:
-#             if colour in word:
-#                 count += 1
-#     return(count)
-
-# with open("colours_20.csv") as csv_file:
-#     reader = csv.reader(csv_file)
-#     print(f'Red: {colour_finder(reader, "red")}', f'Green: {colour_finder(reader, "Green")}', f'Blue: {colour_finder(reader, "Blue")}', f'Yellow: {colour_finder(reader, "Yellow")}')
-
-# or: 
-#     print(f'Red: {colour_finder(reader, "red")}')
-#     print(f'Green: {colour_finder(reader, "green")}'),
-#     print(f'Blue: {colour_finder(reader, "blue")}'),
-#     print(f'Yellow: {colour_finder(reader, "yellow")}')
